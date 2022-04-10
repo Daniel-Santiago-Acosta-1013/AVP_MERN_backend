499b957b40e9a37f30d208f93bca94d64fdd5d11
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2022-04-10 17:38:18 -0500

    conectando la base de datos con express

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv"
+import conectarDB from "./config/db.js";
 
 const app = express();
+dotenv.config()
+
+conectarDB();
 
 app.use('/', (req, res) => {
     res.send("Hola mundo xd xd xd xd xd")
-})
+});
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(4000, () => {
-    console.log("Ervidor funcionado en el puerto 4000")
+    console.log(`Elvidor funcionado en el puerto ${PORT}`);
 });
