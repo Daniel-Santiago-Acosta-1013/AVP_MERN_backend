e07a7533119b6d3e021ca05aa6cad6cc4d72fac8
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2022-04-11 19:13:07 -0500

    Prevenir Registros de Usuarios Duplicados

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,8 +1,27 @@
-const registar = (req, res) => {
+import Veterinario from "../models/Veterinario.js"
 
-    console.log(req.body);
 
-    res.json({ msg: "Resgistrando usuario..." });
+const registar = async (req, res) => {
+    const { email } = req.body;
+
+    // Prevenir usuarios duplicados
+    const existeUsuario = await Veterinario.findOne({ email });
+
+    if (existeUsuario) {
+        const error = new Error("Usuario ya registrado");
+        return res.status(400).json({ msg: error.message });
+    }
+
+    try {
+        // Guaradar un nuevo Veterinario 
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
+        
+        res.json(veterinarioGuardado);
+    } catch (error) {
+        console.log(error);
+    }
+
 };
 const perfil = (req, res) => {
     res.json({ msg: "Mostrando perfil..." });
