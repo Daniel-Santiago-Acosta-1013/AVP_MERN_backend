4dd9c9710c52d0078689c8bccecc9115d2dad144
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2022-04-12 00:29:46 -0500

    Creando Rutas Protegidas en node

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -6,11 +6,12 @@ perfil,
 confirmar,
 autenticar 
 }from '../controllers/veterinarioController.js';
+import checkAuth from "../middleware/authMiddleware.js"
 
 router.post('/', registar);
-router.get('/perfil', perfil);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
+router.get('/perfil', checkAuth, perfil);
 
 export default router;
 
