2057d69c01102c889ea9a1f7b20a2244c3c77744
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2022-04-12 01:50:16 -0500

    Revisando si el usuario que solicita el email existe

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -4,13 +4,22 @@ import {
 registar, 
 perfil, 
 confirmar,
-autenticar 
+autenticar,
+olvidePassword,
+comprobarToken,
+nuevoPassword
 }from '../controllers/veterinarioController.js';
 import checkAuth from "../middleware/authMiddleware.js"
 
+// Area publica 
 router.post('/', registar);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+
+
+// Area privada 
 router.get('/perfil', checkAuth, perfil);
 
 export default router;
